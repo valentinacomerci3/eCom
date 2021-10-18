c7771f2be893de9cf57a1a3e430677c5c0a1ea5c
valentinacomerci3 <67131278+valentinacomerci3@users.noreply.github.com>
2021-10-18 09:27:20 -0700

    general architecture

---
+++ full-cart/src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TitleComponent } from './components/title/title.component';
+
+
+
+@NgModule({
+  declarations: [
+    TitleComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
